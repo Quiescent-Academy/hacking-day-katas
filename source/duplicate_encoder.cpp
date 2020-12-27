02d1984a261b12d22b52640ef27e43decc3d63b2
Jeremy Wright <jeremy.wright@joylabs.com>
2020-12-27 16:52:11 -0700

    My turn is up

--- source/duplicate_encoder.cpp
+++ source/duplicate_encoder.cpp
@@ -1,5 +1,34 @@
 #include <kata/duplicate_encoder.h>
 
+
+
 std::string duplicate_encoder(const std::string& word){
+#if 0
+  //C 
+  for(int i = 0; i < word.size(); ++i) {
+    char c = word[i]
+  }
+
+  //C++98
+  for(std::string::const_iterator i = word.begin();
+  i != word.end();
+  ++i))
+  {
+    char c = *i;
+  }
+
+
+  //C++11
+  for(auto i = std::cbegin(word)); i != std::cend(word); ++i) {
+    char c = *i
+  }
+#endif
+
+  //C++
+  //range based for
+  for(const auto& c : word) {
+    std::cout << c << '\n'
+  }
+
   return word;
 }
